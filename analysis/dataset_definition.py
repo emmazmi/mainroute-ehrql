70734ec4d0d8c4213d61c47f6780a474122df3e0
emmazmi <83311781+emmazmi@users.noreply.github.com>
2023-12-31 20:08:35 +0000

    update definition

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -8,26 +8,6 @@ def make_dataset_colorectal(index_date, end_date):
     
     dataset = Dataset()
 
-    was_adult = (patients.age_on(index_date) >= 16) & (
-    patients.age_on(index_date) <= 110
-    )
-
-    was_alive = (
-    patients.date_of_death.is_after(index_date)
-    | patients.date_of_death.is_null()
-    )
-
-    was_registered = practice_registrations.where(
-        practice_registrations.for_patient_on(index_date)
-        | (practice_registrations.start_date.is_after(index_date) & practice_registrations.start_date.is_on_or_before(end_date))
-    ).exists_for_patient()
-
-    prev_colorectal_ca = clinical_events.where(
-            clinical_events.snomedct_code.is_in(codelists.colorectal_diagnosis_codes_snomed)
-        ).where(
-            clinical_events.date.is_on_or_before(index_date)
-        ).exists_for_patient()
-    
     dataset.reg_date = practice_registrations.where(practice_registrations.start_date.is_on_or_between(index_date, end_date)
                                                     ).sort_by(
                                                         practice_registrations.start_date
