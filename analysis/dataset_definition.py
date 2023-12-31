5003abeaa6bdf55198075efd1f560e9ce7039c96
emmazmi <83311781+emmazmi@users.noreply.github.com>
2023-12-31 20:00:06 +0000

    update definition

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -28,7 +28,10 @@ def make_dataset_colorectal(index_date, end_date):
             clinical_events.date.is_on_or_before(index_date)
         ).exists_for_patient()
     
-    dataset.reg_date = practice_registrations.start_date.where(practice_registrations.start_date.is_on_or_between(index_date, end_date))
+    dataset.reg_date = practice_registrations.where(practice_registrations.start_date.is_on_or_between(index_date, end_date)
+                                                    ).sort_by(
+                                                        practice_registrations.start_date
+                                                    ).first_for_patient().start_date
     
     def has_symptom(codelist):
         return clinical_events.where(clinical_events.snomedct_code.is_in(codelist)
@@ -46,10 +49,22 @@ def make_dataset_colorectal(index_date, end_date):
     dataset.abdopain_symp = has_symptom(codelists.abdopain_codes)
     dataset.anaemia_symp = has_symptom(codelists.anaemia_codes)
 
-    death_date = patients.date_of_death.where(patients.date_of_death.is_on_or_between(index_date, end_date))
-    dereg_date = practice_registrations.end_date.where(practice_registrations.end_date.is_on_or_between(index_date, end_date))
-    colorectal_ca_diag_date = clinical_events.date.where(clinical_events.snomedct_code.is_in(codelists.colorectal_diagnosis_codes_snomed)
-                                                         ).where(clinical_events.date.is_on_or_between(index_date, end_date))
+    death_date = patients.where(patients.date_of_death.is_on_or_between(index_date, end_date)
+                                ).sort_by(
+                                    patients.date_of_death
+                                ).first_for_patient().date_of_death
+
+    dereg_date = practice_registrations.where(practice_registrations.end_date.is_on_or_between(index_date, end_date)
+                                              ).sort_by(
+                                                  practice_registrations.end_date
+                                              ).first_for_patient().end_date
+
+    colorectal_ca_diag_date = clinical_events.where(clinical_events.snomedct_code.is_in(codelists.colorectal_diagnosis_codes_snomed)
+                                                        ).where(
+                                                            clinical_events.date.is_on_or_between(index_date, end_date)
+                                                        ).sort_by(
+                                                            clinical_events.date
+                                                        ).first_for_patient().date
     
     dataset.exit_date = minimum_of(death_date, dereg_date, colorectal_ca_diag_date)
 
