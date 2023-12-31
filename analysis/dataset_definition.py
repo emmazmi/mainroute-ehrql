23a0fdbaf2f7cb15ff14df7a840e35e315f90334
emmazmi <83311781+emmazmi@users.noreply.github.com>
2023-12-31 19:31:46 +0000

    define dataset

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,14 +1,58 @@
-from ehrql import create_dataset
-from ehrql.tables.tpp import patients, practice_registrations
+from ehrql import Dataset, minimum_of
+from ehrql.tables.core import patients, clinical_events
+from ehrql.tables.tpp import practice_registrations
 
-dataset = create_dataset()
+import codelists
 
-index_date = "2020-03-31"
+def make_dataset_colorectal(index_date, end_date):
+    
+    dataset = Dataset()
 
-has_registration = practice_registrations.for_patient_on(
-    index_date
-).exists_for_patient()
+    was_adult = (patients.age_on(index_date) >= 16) & (
+    patients.age_on(index_date) <= 110
+    )
 
-dataset.define_population(has_registration)
+    was_alive = (
+    patients.date_of_death.is_after(index_date)
+    | patients.date_of_death.is_null()
+    )
 
-dataset.sex = patients.sex
+    was_registered = practice_registrations.where(
+        practice_registrations.for_patient_on(index_date).exists_for_patient()
+        | (practice_registrations.start_date.is_after(index_date) & practice_registrations.start_date.is_on_or_before(end_date))
+    )
+
+    prev_colorectal_ca = clinical_events.where(
+            clinical_events.snomedct_code.is_in(codelists.colorectal_diagnosis_codes_snomed)
+        ).where(
+            clinical_events.date.is_on_or_before(index_date)
+        ).exists_for_patient()
+    
+    dataset.reg_date = practice_registrations.start_date.where(practice_registrations.start_date.is_on_or_between(index_date, end_date))
+    
+    def has_symptom(codelist):
+        return clinical_events.where(clinical_events.snomedct_code.is_in(codelist)
+            ).where(
+                clinical_events.date.is_on_or_between(index_date, end_date)
+            ).exists_for_patient()
+    
+    dataset.lowerGI_symp_any = has_symptom(codelists.colorectal_symptom_codes)
+
+    dataset.ida_symp = has_symptom(codelists.ida_codes)
+    dataset.cibh_symp = has_symptom(codelists.cibh_codes)
+    dataset.prbleed_symp = has_symptom(codelists.prbleeding_codes)
+    dataset.wl_symp = has_symptom(codelists.wl_codes)
+    dataset.abdomass_symp = has_symptom(codelists.abdomass_codes)
+    dataset.abdopain_symp = has_symptom(codelists.abdopain_codes)
+    dataset.anaemia_symp = has_symptom(codelists.anaemia_codes)
+
+    death_date = patients.date_of_death.where(patients.date_of_death.is_on_or_between(index_date, end_date)).exists_for_patient()
+    dereg_date = practice_registrations.end_date.where(practice_registrations.end_date.is_on_or_between(index_date, end_date)).exists_for_patient()
+    colorectal_ca_diag_date = clinical_events.date.where(clinical_events.snomedct_code.is_in(codelists.colorectal_diagnosis_codes_snomed)
+                                                         ).where(clinical_events.date.is_on_or_between(index_date, end_date)
+                                                            ).exists_for_patient()
+    
+    dataset.exit_date = minimum_of(death_date, dereg_date, colorectal_ca_diag_date)
+
+    return dataset
+
